111aa5060322f2f624f6167ff9aade7c4953d0b8
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-05-03 13:48:56 -0700

    add a second test for the CreateCount. Did not know how to create to seperate tests so I combined them both into one thing

--- WordCounter.Tests/ControllerTests/WordCounterTest.cs
+++ WordCounter.Tests/ControllerTests/WordCounterTest.cs
@@ -16,14 +16,16 @@ namespace WordCounter.Tests
     ActionResult matchIndex = wordController.Index();
     Assert.IsInstanceOfType(matchIndex, typeof(ViewResult));
     }
-    // Passed: This test method looks to check if this will return the right result.  This is done in order to see if the new controller properly functions with our WordCounter. 
+    // Passed: We are doing two things of this test.  First, This makes sure that the specified object is an instance of the current type.  We specifically want to make sure View is the returned instance of CreateCount. (Making sure that the WordCounterController does its job).  Second, This test method looks to check if this will return the right result.  This is done in order to see if the new controller properly functions with our WordCounter.
     [TestMethod]
     public void CreateCount()
     {
       WordCounterController wordController = new WordCounterController();
+      ActionResult matchIndex = wordController.CreateCount();
       RedirectToActionResult testResult = wordController.CreateCount("Word","Word Wordword word", 2) as RedirectToActionResult;
       string result = testResult.ActionName;
       Assert.AreEqual(result, "Index");
+      Assert.IsInstanceOfType(matchIndex, typeof(ViewResult));
     }
   }
 }
